6707aded2af997a143dc60f54f255771f2a92d4f
Keri Clowes <kerilyn.clowes@gmail.com>
2014-03-11 17:05:07 -0600

    Numeric calculator subtracts two numbers and returns the difference

--- lib/numeric_calculator.rb
+++ lib/numeric_calculator.rb
@@ -2,4 +2,7 @@ class NumericCalculator
   def add(x, y)
     x + y
   end
+  def difference(x, y)
+    x - y 
+  end
 end
--- spec/numeric_calculator_spec.rb
+++ spec/numeric_calculator_spec.rb
@@ -4,11 +4,16 @@ require 'numeric_calculator'
 describe "numeric_calculator" do
   it "adds two numbers and returns the sum" do
     sum = NumericCalculator.new()
-
     expected = 7
     actual = sum.add(3, 4)
     expect(actual).to eq(expected)
   end
+  it "subtracts two numbers and returns the difference" do
+    diff = NumericCalculator.new()
+    expected = 1
+    actual = diff.difference(4, 3)
+    expect(actual).to eq(expected)
+end
 
 end
 
